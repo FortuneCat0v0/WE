201897b0ca6ed543edf2c25dad13d6d60d338eda
Cat <1142094319@qq.com>
2023-06-09 17:46:26 +0800

    导入DOTWeenPro，代码层可以直接引用DOTWeen，但是不能引用部分Module，要把DOTWeen的Module复制一份到代码层，添加引用没有用

---
+++ Unity/Assets/Mono/MonoBehaviour/Item/ItemFader.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+
+// [RequireComponent(typeof (SpriteRenderer))]
+public class ItemFader: MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    /// <summary>
+    /// 逐渐恢复颜色
+    /// </summary>
+    public void FadeIn()
+    {
+        Color targetColor = new Color(1, 1, 1, 1); 
+        // this.spriteRenderer.DOColor()
+    }
+}
